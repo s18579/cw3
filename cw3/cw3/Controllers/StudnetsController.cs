e74eb98ef7197abcb7157695c9ba33fea3e52e31
Mateusz Bednarek <s18579@pjwstk.edu.pl>
2020-03-18 21:53:41 +0100

    init

---
+++ cw3/cw3/Controllers/StudnetsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        [HttpGet("{id}")]
+       public IActionResult GetStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewksi");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+        [HttpPost]
+        public IActionResult CreateStudents(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudents(int id)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudents(int id)
+        {
+            return Ok("Aktualizacja dokonczona");
+        }
+    }
+}
